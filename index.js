dd3db78a6a0f20560f154e368e0c0b093c3ffa9f
Lauren Dostal <laurendostal@yahoo.com>
2024-01-19 19:07:46 -0500

    initial

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+function greet() {
+    let name = prompt("What is your name?");
+
+    alert("Hello " + name + "!");
+
+    let age_as_string = prompt("How old are you?");
+
+    let age = parseInt(age_as_string);
+
+    let birthday_already = confirm("Have you had a birthday this year?");
+
+    let birth_year = new Date().getFullYear()
+
+    if(birthday_already) {
+        birth_year = birth_year - age;
+    }else{
+        birth_year = birth_year - (age + 1);
+    }
+
+    alert("Your birth year is " + birth_year.toString() + "!");
+}
